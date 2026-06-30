cf78867cfcfb7b76f2baaddc2dacd67e210dbf0a
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-01 03:58:38 +0700

    feat: make browser tab title dynamic based on title/header slots

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Posyandu') }}</title>
+        <title>
+            @if(isset($title))
+                {{ $title }} - {{ config('app.name', 'Posyandu') }}
+            @elseif(isset($header))
+                {{ $header }} - {{ config('app.name', 'Posyandu') }}
+            @else
+                {{ config('app.name', 'Posyandu') }}
+            @endif
+        </title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
